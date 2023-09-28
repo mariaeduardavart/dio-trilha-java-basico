1e22e737cffd32de38837f0801b7049105f492b0
mariaeduardavart <dudavart@gmail.com>
2023-09-28 11:43:40 -0300

    criado a classe que receberá dados via terminal de uma conta bancária

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
        
+       Scanner scanner = new Scanner(System.in);
+
+       
+       System.out.print("Digite seu nome: ");
+       String nomeCliente = scanner.nextLine();
+
+       System.out.print("Digite sua agência: ");
+       String agencia = scanner.nextLine();
+
+       System.out.print("Digite o número da sua conta: ");
+       int numero = scanner.nextInt();
+
+      
+       scanner.nextLine();
+
+       System.out.print("Digite seu saldo: ");
+       String saldoStr = scanner.next();
+
+       BigDecimal saldo = new BigDecimal(saldoStr);
+
+       System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+       scanner.close();
     }
 }
+
